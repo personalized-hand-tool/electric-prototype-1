d1e7011f6bb2af5159fac1d66f0490c571a7ab33
joshua-8 <joshua_phelps@brown.edu>
2024-05-01 12:52:33 -0400

    leveling

--- RCMv2/RCMv2.ino
+++ RCMv2/RCMv2.ino
@@ -42,21 +42,6 @@ float motor2Val = 0;
 float motor3Val = 0;
 float motor4Val = 0;
 
-void Enabled()
-{
-    // code to run while enabled, put your main code here
-}
-
-void Enable()
-{
-    // turn on outputs
-}
-
-void Disable()
-{
-    // turn off outputs
-}
-
 void PowerOn()
 {
     // runs once on robot startup, set pin modes and use begin() if applicable here
@@ -70,78 +55,9 @@ void Always()
     // always runs if void loop is running, JMotor run() functions should be put here
     // (but only the "top level", for example if you call drivetrainController.run() you shouldn't also call leftMotorController.run())
     runIMU();
-    rsl_color = voltageComp.getSupplyVoltage() < 7 ? 0xcc0000 : 0xd02d00;
-    servo4Driver.set(imu.roll / 180);
+    rsl_color = voltageComp.getSupplyVoltage() < 7 ? 0xdd0000 : 0x000000;
+        servo4Driver.set(-imu.roll / 90);
     delay(1);
 }
-
-#if RCM_COMM_METHOD == RCM_COMM_EWD
-void WifiDataToParse()
-{
-    enabled = EWD::recvBl();
-    // add data to read here: (EWD::recvBl, EWD::recvBy, EWD::recvIn, EWD::recvFl)(boolean, byte, int, float)
-}
-void WifiDataToSend()
-{
-    EWD::sendFl(voltageComp.getSupplyVoltage());
-    // add data to send here: (EWD::sendBl(), EWD::sendBy(), EWD::sendIn(), EWD::sendFl())(boolean, byte, int, float)
-}
-
-void configWifi()
-{
-    // EWD::mode = EWD::Mode::connectToNetwork;
-    // EWD::routerName = "router";
-    // EWD::routerPassword = "password";
-    // EWD::routerPort = 25210;
-
-    EWD::mode = EWD::Mode::createAP;
-    EWD::APName = "rcm0";
-    EWD::APPassword = "rcmPassword";
-    EWD::APPort = 25210;
-}
-#elif RCM_COMM_METHOD == RCM_COMM_ROS ////////////// ignore everything below this line unless you're using ROS mode/////////////////////////////////////////////
-void ROSWifiSettings()
-{
-    // SSID, password, IP, port (on a computer run: sudo docker run -it --rm --net=host microros/micro-ros-agent:iron udp4 --port 8888 )
-    set_microros_wifi_transports("router", "password", "10.25.21.1", 8888);
-    nodeName = "rcm_robot";
-    // numSubscribers = 10; //change max number of subscribers
-}
-
-#include <example_interfaces/msg/bool.h>
-#include <std_msgs/msg/byte.h>
-#include <std_msgs/msg/float32.h>
-#include <std_msgs/msg/int32.h>
-// and lots of other message types are available (see file available_ros2_types)
-// #include <geometry_msgs/msg/twist.h>
-
-// declare publishers
-declarePub(battery, std_msgs__msg__Float32);
-
-// // declare subscribers and write callback functions
-// declareSubAndCallback(cmd_vel, geometry_msgs__msg__Twist);
-// velCmd.x = cmd_velMsg->linear.x;
-// velCmd.y = cmd_velMsg->linear.y;
-// velCmd.theta = cmd_velMsg->angular.z;
-// } // end of callback
-
-void ROSbegin()
-{
-    // create publishers
-    createPublisher(battery, std_msgs__msg__Float32, "/rcm/battery");
-    batteryMsg.data = 0;
-
-    // add subscribers
-    // addSub(cmd_vel, geometry_msgs__msg__Twist, "/cmd_vel");
-}
-
-void ROSrun()
-{
-    rosSpin(1);
-    // you can add more publishers here
-    batteryMsg.data = voltageComp.getSupplyVoltage();
-    publish(battery);
-}
-#endif
 
 #include "rcmutil.h"
--- RCMv2/rcmutil.h
+++ RCMv2/rcmutil.h
@@ -21,16 +21,6 @@ void setupRSL()
 {
     pixels.begin();
 }
-void enabledRSL()
-{
-#ifndef OVERWRITE_RSL
-    if (millis() % 500 < 250) {
-        setRSL(true);
-    } else {
-        setRSL(false);
-    }
-#endif
-}
 void wifiFailRSL()
 {
 #ifndef OVERWRITE_RSL
@@ -41,23 +31,6 @@ void wifiFailRSL()
     }
 #endif
 }
-void wifiDisconnectedRSL()
-{
-#ifndef OVERWRITE_RSL
-    if (millis() % 1000 >= 100) {
-        setRSL(true);
-    } else {
-        setRSL(false);
-    }
-#endif
-}
-void disabledRSL()
-{
-#ifndef OVERWRITE_RSL
-    setRSL(true);
-#endif
-}
-
 
 void setup()
 {
@@ -65,75 +38,14 @@ void setup()
     setupRSL();
     setupMotors();
     PowerOn();
-    Disable();
-#if RCM_COMM_METHOD == RCM_COMM_EWD
-    configWifi();
-    EWD::setupWifi(WifiDataToParse, WifiDataToSend);
-#elif RCM_COMM_METHOD == RCM_COMM_ROS
-    setupROS();
-#endif
-}
-
-boolean connectedToWifi()
-{
-#if RCM_COMM_METHOD == RCM_COMM_EWD
-    return EWD::wifiConnected;
-#elif RCM_COMM_METHOD == RCM_COMM_ROS
-    return !ROSCheckFail;
-#endif
-}
-boolean connectionTimedOut()
-{
-#if RCM_COMM_METHOD == RCM_COMM_EWD
-    return EWD::timedOut();
-#elif RCM_COMM_METHOD == RCM_COMM_ROS
-    return (millis() - lastEnableSentMillis) > rosWifiTimeout;
-#endif
 }
 
 extern void ROSrun();
 
 void loop()
 {
-#if RCM_COMM_METHOD == RCM_COMM_EWD
-    EWD::runWifiCommunication();
-#elif RCM_COMM_METHOD == RCM_COMM_ROS
-    ROSrun();
-#endif
-    if (!connectedToWifi() || connectionTimedOut()) {
-        enabled = false;
-    }
     Always();
-    if (enabled && !wasEnabled) {
-#if RCM_HARDWARE_VERSION == RCM_BYTE_V2 || RCM_HARDWARE_VERSION == RCM_NIBBLE_V1
-#ifndef RCM_BYTE_DO_NOT_USE_SAFE_DISABLE
-        digitalWrite(motorsEnablePin, HIGH);
-#endif
-#endif
-
-        Enable();
-    }
-    if (!enabled && wasEnabled) {
-        Disable();
-
-#if RCM_HARDWARE_VERSION == RCM_BYTE_V2 || RCM_HARDWARE_VERSION == RCM_NIBBLE_V1
-#ifndef RCM_BYTE_DO_NOT_USE_SAFE_DISABLE
-        digitalWrite(motorsEnablePin, LOW);
-#endif
-#endif
-    }
-    if (enabled) {
-        Enabled();
-        enabledRSL(); //        digitalWrite(ONBOARD_LED, millis() % 500 < 250); // flash, enabled
-    } else {
-        if (!connectedToWifi())
-            wifiFailRSL(); //            digitalWrite(ONBOARD_LED, millis() % 1000 <= 100); // short flash, wifi connection fail
-        else if (connectionTimedOut())
-            wifiDisconnectedRSL(); //            digitalWrite(ONBOARD_LED, millis() % 1000 >= 100); // long flash, no driver station connected
-        else
-            disabledRSL(); //            digitalWrite(ONBOARD_LED, HIGH); // on, disabled
-    }
-    wasEnabled = enabled;
+    wifiFailRSL(); //            digitalWrite(ONBOARD_LED, millis() % 1000 <= 100); // short flash, wifi connection fail
 }
 
 #endif // RCMUTIL_H
